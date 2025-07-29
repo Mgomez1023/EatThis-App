4177da39ea727d86316624b13c2176f741d9ca3b
Martin Gomez <marting2046@gmail.com>
2025-07-28 19:07:13 -0500

    add PWA support

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,28 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss],
-})
+  plugins: [react(), tailwindcss,
+    VitePWA({
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'EatThis App',
+          short_name: 'EatThis',
+          start_url: '/',
+          display: 'standalone',
+          background_color: '#ffffff',
+          theme_color: '#2c3e50',
+          icons: [
+            {
+              src: '/icon-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: '/icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            }
+          ]
+        }
+      })
+    ]})
